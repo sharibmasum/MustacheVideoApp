b339d6ee1e32d88f14852f72a1f506b727a6520e
sharibmasum <sharibmasum@gmail.com>
2025-03-18 00:26:27 -0400

    Initial Commit

---
+++ MustacheVideoApp/MustacheVideoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MustacheVideoAppApp.swift
+//  MustacheVideoApp
+//
+//  Created by sharib masum on 2025-03-18.
+//
+
+import SwiftUI
+
+@main
+struct MustacheVideoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
